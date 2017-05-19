e4a055e2cdd5c48fff869f062baea37a36c31ce8
Derek Gonyeo <derek@gonyeo.com>
2017-05-19 12:07:42 -0700

    internal/providers/azure: ssh key support
    This commit modifies the azure provider to add support for fetching an
    ssh key. A comment in internal/providers/azure/azure.go explains the
    process for how this is performed.

--- internal/providers/azure/azure.go
+++ internal/providers/azure/azure.go
@@ -18,12 +18,15 @@ import (
 	"bufio"
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"os"
+	"path"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/coreos/coreos-metadata/internal/exec"
 	"github.com/coreos/coreos-metadata/internal/providers"
 	"github.com/coreos/coreos-metadata/internal/retry"
 )
@@ -63,6 +66,14 @@ type GoalStateRoleInstance struct {
 	ConfigName               string   `xml:"Configuration>ConfigName"`
 }
 
+type certResponse struct {
+	XMLName     xml.Name `xml:"CertificateFile"`
+	Version     string   `xml:"Version"`
+	Incarnation string   `xml:"Incarnation"`
+	Format      string   `xml:"Format"`
+	Data        string   `xml:"Data"`
+}
+
 func FetchMetadata() (providers.Metadata, error) {
 	addr, err := getFabricAddress()
 	if err != nil {
@@ -83,11 +94,17 @@ func FetchMetadata() (providers.Metadata, error) {
 		return providers.Metadata{}, err
 	}
 
+	sshKey, err := fetchSshKey(goal)
+	if err != nil {
+		return providers.Metadata{}, err
+	}
+
 	return providers.Metadata{
 		Attributes: map[string]string{
 			"AZURE_IPV4_DYNAMIC": providers.String(config.dynamicIPv4),
 			"AZURE_IPV4_VIRTUAL": providers.String(config.virtualIPv4),
 		},
+		SshKeys: []string{sshKey},
 	}, nil
 }
 
@@ -259,4 +276,159 @@ func fetchSharedConfig(goal GoalState) (metadata, error) {
 	}
 
 	return config, nil
+}
+
+// fetchSshKey will fetch a single ssh key from azure and return either it or an
+// error
+//
+// The endpoint to fetch the ssh key from is stored in the GoalState under the
+// "Certificates" field. The process for how to access this endpoint and produce
+// an ssh key from the result goes as follows:
+// - generate a local rsa 2048 bit key pair
+// - provide the public key when making the request to the certificates endpoint
+// - take the contents of the "Data" field in the xml from the endpoint
+// - prepend some lines to this content to make it a valid file for openssl
+// - use the private key we generated to decrypt this value
+// - extract a public key from the decrypted contents in a pkcs8 format
+// - use ssh-keygen to convert this public key into the ssh format
+//
+// fetchSshKey can only handle a single ssh key.
+func fetchSshKey(goal GoalState) (string, error) {
+	if len(goal.RoleInstanceList) == 0 {
+		return "", fmt.Errorf("role instance list empty in goal state")
+	}
+
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		return "", err
+	}
+	defer os.RemoveAll(tmpDir)
+
+	keyFilePath := path.Join(tmpDir, "TransportPrivate.pem")
+	certFilePath := path.Join(tmpDir, "TransportCert.pem")
+
+	err = genCerts(keyFilePath, certFilePath)
+	if err != nil {
+		return "", err
+	}
+
+	formattedCert, err := prepareCertForHeader(certFilePath)
+	if err != nil {
+		return "", err
+	}
+
+	client := getClient()
+	client.Header["x-ms-cipher-name"] = []string{"DES_EDE3_CBC"}
+	client.Header["x-ms-guest-agent-public-x509-cert"] = []string{formattedCert}
+
+	certResponseBlob, err := client.Get(goal.RoleInstanceList[0].Certificates)
+	if err != nil {
+		return "", err
+	}
+
+	certResponse := certResponse{}
+	err = xml.Unmarshal(certResponseBlob, &certResponse)
+	if err != nil {
+		return "", err
+	}
+
+	encryptedSshCertFile := path.Join(tmpDir, "encrypted-ssh-cert.pem")
+	err = ioutil.WriteFile(encryptedSshCertFile, []byte(addP7mHeader(certResponse.Data, encryptedSshCertFile)), 0644)
+	if err != nil {
+		return "", err
+	}
+
+	sshCertFile := path.Join(tmpDir, "decrypted-ssh-cert.pem")
+	err = decryptFile(encryptedSshCertFile, sshCertFile, keyFilePath, certFilePath)
+	if err != nil {
+		return "", err
+	}
+
+	sshPubkeyFile := path.Join(tmpDir, "ssh-pubkey.pem")
+	err = producePubkey(sshCertFile, sshPubkeyFile)
+	if err != nil {
+		return "", err
+	}
+
+	pubkey, err := getSshPubkey(sshPubkeyFile)
+	if err != nil {
+		return "", err
+	}
+	return pubkey, nil
+}
+
+// genCerts will generate a new 2048 bit rsa key and certificate for use in
+// certificate retrieval.
+func genCerts(keyFilePath, certFilePath string) error {
+	err := exec.ExecExpectZero("openssl", "req", "-x509", "-nodes", "-subj", "/CN=LinuxTransport", "-days", "32768", "-newkey", "rsa:2048", "-keyout", keyFilePath, "-out", certFilePath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func prepareCertForHeader(certFilePath string) (string, error) {
+	contents, err := ioutil.ReadFile(certFilePath)
+	if err != nil {
+		return "", err
+	}
+	lines := strings.Split(string(contents), "\n")
+	res := ""
+	for _, line := range lines {
+		if line == "-----BEGIN CERTIFICATE-----" || line == "-----END CERTIFICATE-----" {
+			continue
+		}
+		res += line
+	}
+	return res, nil
+}
+
+func addP7mHeader(str, filePath string) string {
+	output := "MIME-Version:1.0\n"
+	output += fmt.Sprintf("Content-Disposition: attachment; filename=%q\n", filePath)
+	output += fmt.Sprintf("Content-Type: application/x-pkcs7-mime; name=%q\n", filePath)
+	output += "Content-Transfer-Encoding: base64\n"
+	output += "\n"
+	output += str
+	return output
+}
+
+func decryptFile(in, out, keyFilePath, certFilePath string) error {
+	stdout, stderr, code, err := exec.ExecNoStdin("openssl", "cms", "-decrypt", "-in", in, "-inkey", keyFilePath, "-recip", certFilePath)
+	if err != nil {
+		return err
+	}
+	if code != 0 {
+		return fmt.Errorf("couldn't decrypt certificates: %s", stderr)
+	}
+	stdout, stderr, code, err = exec.Exec(stdout, "openssl", "pkcs12", "-nodes", "-password", "pass:", "-out", out)
+	if err != nil {
+		return err
+	}
+	if code != 0 {
+		return fmt.Errorf("couldn't decrypt certificates: %s", stderr)
+	}
+	return nil
+}
+
+func producePubkey(certFile, pubkeyFile string) error {
+	stdout, stderr, code, err := exec.ExecNoStdin("openssl", "x509", "-in", certFile, "-pubkey", "-noout")
+	if err != nil {
+		return err
+	}
+	if code != 0 {
+		return fmt.Errorf("couldn't produce pubkey: %s", stderr)
+	}
+	return ioutil.WriteFile(pubkeyFile, []byte(stdout), 0644)
+}
+
+func getSshPubkey(pubkeyFile string) (string, error) {
+	stdout, stderr, code, err := exec.ExecNoStdin("ssh-keygen", "-i", "-m", "PKCS8", "-f", pubkeyFile)
+	if err != nil {
+		return "", err
+	}
+	if code != 0 {
+		return "", fmt.Errorf("couldn't extract pubkey: %s", stderr)
+	}
+	return stdout, nil
 }
