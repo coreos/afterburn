04ff629f34352364a7e6d9902868391e1bd798df
Stephen Demos <stphndemos@gmail.com>
2017-07-21 17:52:06 -0700

    main: simple cli mimicking golang coreos-metadata
    this implementation just implements the command-line tool. it is mostly
    not implemented still.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,115 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate slog;
+extern crate slog_term;
+extern crate slog_async;
+#[macro_use]
+extern crate slog_scope;
+
+use std::fs::File;
+use std::io::prelude::*;
+use clap::{Arg, App};
+use slog::Drain;
+
+const CMDLINE_PATH: &'static str = "/proc/cmdline";
+const CMDLINE_OEM_FLAG:&'static str = "coreos.oem.id";
+
+#[derive(Debug)]
+struct Config {
+    provider: String,
+    attributes_file: Option<String>,
+    ssh_keys_user: Option<String>,
+    hostname_file: Option<String>,
+    network_units_dir: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // setup logging
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    let log = slog::Logger::root(drain, slog_o!());
+    let _guard = slog_scope::set_global_logger(log);
+
+    debug!("Logging initialized");
+
+    let config = init().unwrap();
+    info!("{:?}", config)
+}
+
+fn init() -> Result<Config, String> {
+    // setup cli
+    let matches = App::new("coreos-metadata")
+        .version(crate_version!())
+        .arg(Arg::with_name("attributes")
+             .long("attributes")
+             .help("The file into which the metadata attributes are written")
+             .takes_value(true))
+        .arg(Arg::with_name("cmdline")
+             .long("cmdline")
+             .help("Read the cloud provider from the kernel cmdline"))
+        .arg(Arg::with_name("hostname")
+             .long("hostname")
+             .help("The file into which the hostname should be written")
+             .takes_value(true))
+        .arg(Arg::with_name("network-units")
+             .long("network-units")
+             .help("The directory into which network units are written")
+             .takes_value(true))
+        .arg(Arg::with_name("provider")
+             .long("provider")
+             .help("The name of the cloud provider")
+             .takes_value(true))
+        .arg(Arg::with_name("ssh-keys")
+             .long("ssh-keys")
+             .help("Update SSH keys for the given user")
+             .takes_value(true))
+        .get_matches();
+
+    // return configuration
+    Ok(Config {
+        provider: match matches.value_of("provider") {
+            Some(provider) => String::from(provider),
+            None => if matches.is_present("cmdline") {
+                get_oem()?
+            } else {
+                return Err("Must set either --provider or --cmdline".to_string());
+            }
+        },
+        attributes_file: matches.value_of("attributes").map(String::from),
+        ssh_keys_user: matches.value_of("ssh-keys").map(String::from),
+        hostname_file: matches.value_of("hostname").map(String::from),
+        network_units_dir: matches.value_of("network-units").map(String::from),
+    })
+}
+
+fn get_oem() -> Result<String, String> {
+    // open the cmdline file
+    let mut file = File::open(CMDLINE_PATH)
+        .map_err(|err| format!("Failed to open cmdline file ({}) - {}", CMDLINE_PATH, err))?;
+
+    // read the contents
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .map_err(|err| format!("Failed to read cmdline file ({}) - {}", CMDLINE_PATH, err))?;
+
+    // split the contents into elements
+    let params: Vec<Vec<&str>> = contents.split(' ')
+        .map(|s| s.split('=').collect())
+        .collect();
+
+    // find the oem flag
+    for p in params {
+        if p.len() > 1 && p[0] == CMDLINE_OEM_FLAG {
+            return Ok(String::from(p[1]));
+        }
+    }
+
+    // if we didn't find the oem flag then explode
+    // panic!("Couldn't find '{}' flag in cmdline file ({})", CMDLINE_OEM_FLAG, CMDLINE_PATH)
+    Err(format!("Couldn't find '{}' flag in cmdline file ({})", CMDLINE_OEM_FLAG, CMDLINE_PATH))
 }
