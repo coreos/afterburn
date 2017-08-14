57ef2199419c6ff49caaa3a93f5fa797835a1ede
Alexander Pavel <alex.pavel@coreos.com>
2017-08-14 16:49:01 -0700

    review: improve description for set flag

--- internal/main.go
+++ internal/main.go
@@ -81,7 +81,7 @@ func main() {
 	flag.StringVar(&flags.hostname, "hostname", "", "The file into which the hostname should be written")
 	flag.StringVar(&flags.networkUnits, "network-units", "", "The directory into which network units are written")
 	flag.StringVar(&flags.provider, "provider", "", "The name of the cloud provider")
-	flag.Var(&flags.overrides, "set", "Set an environment variable, overriding the default/detected variable")
+	flag.Var(&flags.overrides, "set", "Set a metadata attribute, potentially overriding the attribute given by a provider. This flag can be specified multiple times and expects a key=value pair. Eg: VIRTUALBOX_IPV4_PRIVATE=172.17.4.101")
 	flag.StringVar(&flags.sshKeys, "ssh-keys", "", "Update SSH keys for the given user")
 	flag.BoolVar(&flags.version, "version", false, "Print the version and exit")
 
