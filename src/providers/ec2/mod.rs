749b8b85789f6e1e7eb16aea42ca2d658c2561db
Derek Gonyeo <derek@gonyeo.com>
2017-08-23 17:14:13 -0700

    providers/ec2: add ssh keys implementation
    this gets ssh public keys from the ec2 metadata service

--- src/providers/ec2/mod.rs
+++ src/providers/ec2/mod.rs
@@ -43,6 +43,8 @@ pub fn fetch_metadata() -> Result<Metadata> {
     let region: Option<String> = client.get(retry::Json, url_for_key("dynamic/instance-identity/document")).send()?
         .map(|instance_id_doc: InstanceIdDoc| instance_id_doc.region);
 
+    let mut ssh_keys: Vec<String> = fetch_ssh_keys(client)?;
+
     Ok(Metadata::builder()
         .add_attribute_if_exists("EC2_REGION".to_owned(), region)
         .add_attribute_if_exists("EC2_INSTANCE_ID".to_owned(), instance_id)
@@ -51,5 +53,32 @@ pub fn fetch_metadata() -> Result<Metadata> {
         .add_attribute_if_exists("EC2_HOSTNAME".to_owned(), hostname.clone())
         .add_attribute_if_exists("EC2_AVAILABILITY_ZONE".to_owned(), availability_zone)
         .set_hostname_if_exists(hostname)
+        .add_ssh_keys(ssh_keys)
         .build())
+}
+
+fn fetch_ssh_keys(client: retry::Client) -> Result<Vec<String>> {
+    let keydata: Option<String> = client.get(retry::Raw, url_for_key("meta-data/public-keys")).send()?;
+    match keydata {
+        Some(s) => {
+            let mut res = Ok(Vec::new());
+            for keyname in s.split_whitespace() {
+                let tokens: Vec<&str> = keyname.split('=').collect();
+                if tokens.len() != 2 {
+                    res = Err("malformed public key".into());
+                    break;
+                }
+                let ssh_key: String = client.get(retry::Raw, url_for_key(&format!("meta-data/public-keys/{}/openssh-key", tokens[0]))).send()?
+                    .ok_or("missing ssh key")?;
+                match res.as_mut() {
+                    Ok(keys) => (*keys).push(ssh_key),
+                    Err(_) => (),
+                }
+            }
+            res
+        }
+        None => {
+            Ok(Vec::new())
+        }
+    }
 }
