fe2703497ef2b961acf705b2bc2c85f1a230a228
Luca Bruno <luca.bruno@coreos.com>
2018-11-05 20:07:47 +0000

    cargo: update nix to 0.11

--- src/providers/cloudstack/configdrive.rs
+++ src/providers/cloudstack/configdrive.rs
@@ -84,7 +84,7 @@ impl ConfigDrive {
     }
 
     fn mount_ro(source: &Path, target: &Path, fstype: &str) -> Result<()> {
-        mount::mount(Some(source), target, Some(fstype), mount::MS_RDONLY, None::<&str>)
+        mount::mount(Some(source), target, Some(fstype), mount::MsFlags::MS_RDONLY, None::<&str>)
             .chain_err(|| format!("failed to read-only mount source '{:?}' to target '{:?}' with filetype '{}'", source, target, fstype))
     }
 
