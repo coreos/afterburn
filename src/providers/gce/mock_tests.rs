161d7660c5f564eb77a81acfcdc7d5701173e29c
Luca Bruno <luca.bruno@coreos.com>
2019-03-19 10:31:40 +0000

    providers/gce: add basic hostname mock-test
    This adds a simple mock-test covering hostname fetching.

---
+++ src/providers/gce/mock_tests.rs
@@ -0,0 +1,29 @@
+use errors::*;
+use mockito;
+use providers::gce;
+use providers::MetadataProvider;
+
+#[test]
+fn basic_hostname() {
+    let ep = "/instance/hostname";
+    let hostname = "test-hostname";
+
+    let mut provider = gce::GceProvider::try_new().unwrap();
+    provider.client = provider.client.max_attempts(1);
+
+    provider.hostname().unwrap_err();
+
+    let _m = mockito::mock("GET", ep).with_status(503).create();
+    provider.hostname().unwrap_err();
+
+    let _m = mockito::mock("GET", ep)
+        .with_status(200)
+        .with_body(hostname)
+        .create();
+    let v = provider.hostname().unwrap();
+    assert_eq!(v, Some(hostname.to_string()));
+
+    let _m = mockito::mock("GET", ep).with_status(404).create();
+    let v = provider.hostname().unwrap();
+    assert_eq!(v, None);
+}
