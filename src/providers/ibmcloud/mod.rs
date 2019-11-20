8cb6e4aa6a84fdb520b653796ac2d851afb4e386
Luca BRUNO <luca.bruno@coreos.com>
2019-11-20 13:47:48 +0000

    providers/ibmcloud: add support for Classic instance types
    This adds initial metadata support for IBM Cloud Classic instance
    types.
    Right now it only supports extracting attributes and exposing the
    hostname.

--- src/providers/ibmcloud/mod.rs
+++ src/providers/ibmcloud/mod.rs
@@ -3,22 +3,27 @@
 //! IBMCloud supports multiple kind of compute nodes, with different
 //! features and peculiarities.
 
-// TODO(lucab): this allows adding 'classic' and 'vpc-gen1' instances too
-//  via auto-detection, if there is a need for that in the future.
+// TODO(lucab): this allows adding 'vpc-gen1' instances too via auto-detection,
+//  if there is a need for that in the future.
 
 use crate::errors::Result;
 use crate::providers;
 
+mod classic;
 mod gen2;
 
 /// Build a new client for IBMCloud.
 ///
 /// This internally tries to autodetect the infrastructure type
 /// and mount the relevant config-drive.
+/// Auto-detection order: Gen2, classic (possibly later: Gen1).
 pub fn try_autodetect() -> Result<Box<dyn providers::MetadataProvider>> {
-    // Auto-detection order: Gen2 (possibly later: classic, Gen1).
     slog_scope::trace!("trying to autodetect ibmcloud infrastructure type");
 
+    // TODO(lucab): make auto-detection parallel (and cancellable), otherwise
+    //  Classic may suffer some noticeable delay.
+
+    // First try: Gen2.
     match gen2::G2Provider::try_new() {
         Ok(g2) => {
             slog_scope::info!("found metadata for VPC-Gen2 instance");
@@ -26,6 +31,17 @@ pub fn try_autodetect() -> Result<Box<dyn providers::MetadataProvider>> {
         }
         Err(e) => {
             slog_scope::debug!("ibmcloud VPC-Gen2 autodetection failed: {}", e);
+        }
+    };
+
+    // Second try: Classic.
+    match classic::ClassicProvider::try_new() {
+        Ok(classic) => {
+            slog_scope::info!("found metadata for Classic instance");
+            return Ok(Box::new(classic));
+        }
+        Err(e) => {
+            slog_scope::debug!("ibmcloud Classic autodetection failed: {}", e);
         }
     };
 
