aaf746cb9ef16b940262e251e2a3f63d924e8551
Luca BRUNO <luca.bruno@coreos.com>
2020-01-20 12:21:17 +0000

    providers: add experimental initrd network bootstrap
    This adds initial/experimental support for bootstrapping the network
    in the initrd. It is meant to support weird cloud providers where
    DHCP is not available or not usable.
    This feature is currently reachable as a dedicated `exp rd-net-bootstrap`
    subcommand.
    The first provider where this logic is required is `ibmcloud-classic`.

--- dracut/30afterburn/module-setup.sh
+++ dracut/30afterburn/module-setup.sh
@@ -16,9 +16,14 @@ install() {
     inst_simple "$moddir/afterburn-hostname.service" \
         "$systemdutildir/system/afterburn-hostname.service"
 
+    inst_simple "$moddir/afterburn-net-bootstrap.service" \
+        "$systemdutildir/system/afterburn-net-bootstrap.service"
+
     # We want the afterburn-hostname to be firstboot only, so Ignition-provided
     # hostname changes do not get overwritten on subsequent boots
-
     mkdir -p "$initdir/$systemdsystemunitdir/ignition-complete.target.requires"
     ln -s "../afterburn-hostname.service" "$initdir/$systemdsystemunitdir/ignition-complete.target.requires/afterburn-hostname.service"
+
+    mkdir -p "$initdir/$systemdsystemunitdir/ignition-fetch.service.requires"
+    ln -s "../afterburn-net-boostrap.service" "$initdir/$systemdsystemunitdir/ignition-fetch.service.requires/afterburn-net-bootstrap.service"
 }
