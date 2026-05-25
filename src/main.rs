d4869783183349463a4cc7dc01cb01ffd1edfa8c
Yasmin de Souza <ydesouza@redhat.com>
2026-05-25 10:48:49 -0300

    main: Constrain slog for Fedora and fix o! macro
    error: use of deprecated macro : Use fully qualified macro slog::o!(...) instead

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ mod retry;
 mod util;
 
 use anyhow::{Context, Result};
-use slog::{slog_o, Drain};
+use slog::Drain;
 use slog_scope::debug;
 use std::env;
 
@@ -30,7 +30,7 @@ fn main() -> Result<()> {
     let decorator = slog_term::TermDecorator::new().build();
     let drain = slog_term::FullFormat::new(decorator).build().fuse();
     let drain = slog_async::Async::new(drain).build().fuse();
-    let log = slog::Logger::root(drain, slog_o!());
+    let log = slog::Logger::root(drain, slog::o!());
     let _guard = slog_scope::set_global_logger(log);
     debug!("logging initialized");
 
